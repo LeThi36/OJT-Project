8497027924d608521b37fe0b011f56109a3fd8cc
bangd1nh <dinhbang121@gmail.com>
2024-10-09 08:48:57 +0700

    first-commit

---
+++ lm-backend/src/main/java/ojt/lm_backend/LmBackendApplication.java
@@ -0,0 +1,13 @@
+package ojt.lm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmBackendApplication.class, args);
+	}
+
+}
